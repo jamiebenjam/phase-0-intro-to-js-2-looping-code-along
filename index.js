31e62049b231cb73316ab226c2b4bac3c19a16b6
Jamie Benjamin <bennythedev@gmail.com>
2022-06-08 23:08:53 -0500

    commit tree 76f305e1c98376d9e8bc71a1dc15dc813b1b8ce2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+//writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function writeCards(name, adjective) {
+    let greeting = []
+    for (let i = 0; i < name.length; i++) {
+        greeting.push(`Thank you, ${name[i]}, for the wonderful ${adjective} gift!`);
+    } 
+    return greeting;
+}
+
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    };
+}
